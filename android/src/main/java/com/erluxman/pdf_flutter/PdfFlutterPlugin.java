afde37632da6bf366167998228afbc05e02d6dda
KhaledShazbek <49252519+KhaledShazbek@users.noreply.github.com>
2022-06-16 10:36:07 +0300

    Update PdfFlutterPlugin.java

--- android/src/main/java/com/erluxman/pdf_flutter/PdfFlutterPlugin.java
+++ android/src/main/java/com/erluxman/pdf_flutter/PdfFlutterPlugin.java
@@ -3,7 +3,7 @@ package com.erluxman.pdf_flutter;
 import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 /** PdfFlutterPlugin */
-public class PdfFlutterPlugin {
+public class PdfFlutterPlugin implements FlutterPlugin {
   public static void registerWith(Registrar registrar) {
     registrar
             .platformViewRegistry()
